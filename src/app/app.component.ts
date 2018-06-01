bad7a136cb978927d10bd0a7d83ee8c502409b13
Caique Vallim <caiquevallim@hotmail.com>
2018-05-31 21:21:46 -0300

    Creating files angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+<<<<<<< HEAD
   styleUrls: ['../style.scss']
+=======
+  styleUrls: ['./app.component.css']
+>>>>>>> 1f9d723... initial commit
 })
 export class AppComponent {
   title = 'app';
