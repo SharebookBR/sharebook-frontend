bb0a375700ea3ac92afe6dc2906774bf2d18b859
Raffaello Damgaard <raffacabofrio@gmail.com>
2018-06-23 19:14:22 -0300

    Componentes header, footer e home.

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['../../style.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
