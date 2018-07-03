8dc0c313e6598f3a438203c3936493d1272f3d65
walter-lopes <walter_vinicius@outlook.com>
2018-07-03 11:28:32 -0300

    corrigindo o jwt interceptor

--- src/app/core/helpers/jwt.interceptor.ts
+++ src/app/core/helpers/jwt.interceptor.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { browser } from 'protractor';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         // add authorization header with jwt token if available
         const shareBookUser = JSON.parse(localStorage.getItem('shareBookUser'));
-        if (shareBookUser && shareBookUser.token) {
+        if (shareBookUser && shareBookUser.accessToken) {
             request = request.clone({
                 setHeaders: {
-                    Authorization: `Bearer ${shareBookUser.token}`
+                    Authorization: `Bearer ${shareBookUser.accessToken}`
                 }
             });
         }
