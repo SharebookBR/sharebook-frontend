c3ed47e079b5ae2a924d7f2d6acdef4b28c29fb8
wantero <wantero@dlctec.com.br>
2018-08-09 00:11:56 -0300

    alterado o modal para listar os usuários para doação

--- src/app/components/book/donate/donate.component.ts
+++ src/app/components/book/donate/donate.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+import { Component, OnInit, Input } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { LocalDataSource } from 'ng2-smart-table';
+
+import { BookService } from '../../../core/services/book/book.service';
 
 
 @Component({
@@ -8,10 +11,70 @@ import { NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./donate.component.css']
 })
 export class DonateComponent implements OnInit {
+  @Input() bookId;
+  donateUsers: LocalDataSource;
+  settings: any;
+  isLoading: Boolean = true;
 
-  constructor(public activeModal: NgbActiveModal) { }
+  constructor(
+    public activeModal: NgbActiveModal,
+    private _scBook: BookService) {
+  }
 
   ngOnInit() {
+    this._scBook.getGranteeUsersByBookId(this.bookId).subscribe(resp => {
+      this.donateUsers = new LocalDataSource(<any>resp);
+      this.isLoading = false;
+    });
+
+    const btnDonate = '<span class="btn btn-warning btn-sm"> <i class="fa fa-trophy"></i> </span>';
+
+    this.settings = {
+      mode: 'inline',
+      hideSubHeader: true,
+      columns: {
+        name: {
+          title: 'Nome',
+          filter: false
+        },
+        email: {
+          title: 'E-mail',
+          filter: false
+        },
+        /*linkedin: {
+          title: 'LinkedIn',
+          filter: false
+        },
+        phone: {
+          title: 'Telefone',
+          valuePrepareFunction: data => data ? data.phone : '',
+          filter: false
+        },
+        postalCode: {
+          title: 'Cep',
+          filter: false
+        }*/
+      },
+      actions: {
+        delete: false,
+        edit: false,
+        add: false,
+        update: false,
+        custom: [
+          {
+            name: 'donate',
+            title: btnDonate,
+          }
+        ],
+        position: 'right', // left|right
+      },
+    };
+  }
+
+  onCustom(event) {
+    if (event.action === 'donate') {
+      console.log(event.data.name);
+    }
   }
 
 }
