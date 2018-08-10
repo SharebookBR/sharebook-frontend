929a87612db1de9ef0cb22fa5772cd6b94b20b6c
wantero <wantero@dlctec.com.br>
2018-08-10 20:45:34 -0300

    Correção no HttpInterceptor para mensagens de erro.

--- src/app/core/helpers/error.interceptor.ts
+++ src/app/core/helpers/error.interceptor.ts
@@ -7,18 +7,18 @@ import { AuthenticationService } from '../services/authentication/authentication
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-    constructor(private authenticationService: AuthenticationService) {}
+  constructor(private authenticationService: AuthenticationService) {}
 
-    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        return next.handle(request).pipe(catchError(err => {
-            if (err.status === 401) {
-                // auto logout if 401 response returned from api
-                this.authenticationService.logout();
-                location.reload(true);
-            }
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(request).pipe(catchError(err => {
+      if (err.status === 401) {
+        // auto logout if 401 response returned from api
+        this.authenticationService.logout();
+        location.reload(true);
+      }
 
-            const error = err.error.message || err.statusText;
-            return throwError(error);
-        }));
-    }
+      const error = err.error.messages || err.statusText;
+      return throwError(error);
+    }));
+  }
 }
