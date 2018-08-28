aca63498130a75a137d01c76d22793a6fcd3dcba
wantero <wantero@dlctec.com.br>
2018-08-28 12:53:55 -0300

    Pattern do LinkedIn mais amigável

--- src/app/core/utils/app.const.ts
+++ src/app/core/utils/app.const.ts
@@ -1,4 +1,4 @@
-export const linkedInUrlPattern = '^https:\/\/[a-z]{2,3}\.linkedin\.com\/.*$';
+export const linkedInUrlPattern = '^(https?:\/\/)?([a-z]{2,3}\.)?linkedin\.com\/.*$';
 export const postalCodePattern  = '^\\d{5}[-]\\d{3}$';
 export const phonePattern       = '^\\([0-9]{2}\\) [0-9]{4,5}-[0-9]{4}$';
 export const emailPattern       = '[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}$';
