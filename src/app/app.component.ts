eb664eff538493e5bcb128ae2a3b8c33331240b4
Thainan Matias <thainan@dynavideo.com.br>
2018-09-10 09:26:53 -0300

    [AmILogged] Implement local storage user removal on token expiration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['../style.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  localStorageUserKey = 'shareBookUser';
+
+  ngOnInit() {
+    this.checkTokenValidity();
+  }
+
+  checkTokenValidity() {
+    const user = JSON.parse(localStorage.getItem(this.localStorageUserKey));
+    if (user) {
+      const expiration = moment(user.expiration).utc();
+      const now = moment().utc();
+      if(now.isAfter(expiration)) localStorage.setItem(this.localStorageUserKey, null);
+    }
+  }
 }
