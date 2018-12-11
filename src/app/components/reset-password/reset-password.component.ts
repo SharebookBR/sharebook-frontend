722096020d00bad1a58b1c3a45e8e3e9f7778ab8
wantero <wantero@dlctec.com.br>
2018-12-11 01:08:39 -0200

    Alterações no reset-password para atender a solicitação de resetar senha

--- src/app/components/reset-password/reset-password.component.ts
+++ src/app/components/reset-password/reset-password.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormArray, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { UserService } from '../../core/services/user/user.service';
+import { AlertService } from '../../core/services/alert/alert.service';
+import * as AppConst from '../../core/utils/app.const';
+
 
 @Component({
   selector: 'app-reset-password',
@@ -7,9 +14,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResetPasswordComponent implements OnInit {
 
-  constructor() { }
+  formGroup: FormGroup;
+
+  constructor(
+    private _scUser: UserService,
+    private _scAlert: AlertService,
+    private _router: Router,
+    private _formBuilder: FormBuilder
+  ) {
+    this.formGroup = _formBuilder.group({
+      email: ['', [Validators.required, Validators.pattern(AppConst.emailPattern)]]
+    });
+  }
 
   ngOnInit() {
+  }
+
+  resetPassword() {
+    if (this.formGroup.valid) {
+      this._scUser.resetPassword(this.formGroup.value.email).subscribe(
+        data => {
+          console.log(data.successMessage);
+          if (data.success || data.authenticated) {
+            alert(data.successMessage);
+            this._router.navigate(['/panel']);
+          } else {
+            this._scAlert.error(data.messages[0]);
+          }
+        },
+        error => {
+          this._scAlert.error(error);
+        }
+      );
+    }
   }
 
 }
