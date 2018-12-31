209aec47ee1bf5fd8b0c7676d6bfc58c0b193109
gilvan <g.r.almeida@live.com>
2018-12-31 20:29:49 -0200

    Adicionando os Models Contributor e Link para tipar o retorno do metodo getContributors do serviço Contributors. Remoção do código html hard coded, para a utilização do serviço

---
+++ src/app/core/models/contributor.ts
@@ -0,0 +1,8 @@
+import {Link} from './link';
+
+export class Contributor {
+  image: string;
+  name: string;
+  position: string;
+  links: Link[];
+}
---
+++ src/app/core/models/link.ts
@@ -0,0 +1,4 @@
+export class Link {
+  url: string;
+  content: any;
+}
