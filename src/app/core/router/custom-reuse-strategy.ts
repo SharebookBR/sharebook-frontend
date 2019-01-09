8b1da723554cd70281d49360b9607a1f4afe9b74
wantero <wantero@dlctec.com.br>
2019-01-09 01:38:05 -0200

    alterado ReuseStrategy para cachear somente o book/list

--- src/app/core/router/custom-reuse-strategy.ts
+++ src/app/core/router/custom-reuse-strategy.ts
@@ -1,44 +1,35 @@
-// Credits to https://www.softwarearchitekt.at/post/2016/12/02/sticky-routes-in-angular-2-3-with-routereusestrategy.aspx
-
-import {RouteReuseStrategy, DefaultUrlSerializer, ActivatedRouteSnapshot, DetachedRouteHandle} from '@angular/router';
+import { ActivatedRouteSnapshot, DetachedRouteHandle, RouteReuseStrategy } from '@angular/router';
 
 export class CustomReuseStrategy implements RouteReuseStrategy {
 
-    handlers: {[key: string]: DetachedRouteHandle} = {};
+    handlers: { [key: string]: DetachedRouteHandle } = {};
 
-    calcKey(route: ActivatedRouteSnapshot) {
-        let next = route;
-        let url = '';
-        while (next) {
-            if (next.url) {
-                url = next.url.join('/');
-            }
-            next = next.firstChild;
-        }
-        return url;
+    /** Determines if this route (and its subtree) should be detached to be reused later */
+    public shouldDetach(route: ActivatedRouteSnapshot): boolean {
+        return (route.routeConfig.path === 'book/list');
     }
 
-    shouldDetach(route: ActivatedRouteSnapshot): boolean {
-        return true;
+    /** Stores the detached route */
+    public store(route: ActivatedRouteSnapshot, handle: DetachedRouteHandle): void {
+        this.handlers[route.routeConfig.path] = handle;
     }
 
-    store(route: ActivatedRouteSnapshot, handle: DetachedRouteHandle): void {
-        this.handlers[this.calcKey(route)] = handle;
+    /** Determines if this route (and its subtree) should be reattached */
+    public shouldAttach(route: ActivatedRouteSnapshot): boolean {
+        return !!route.routeConfig && !!this.handlers[route.routeConfig.path];
     }
 
-    shouldAttach(route: ActivatedRouteSnapshot): boolean {
-        return !!route.routeConfig && !!this.handlers[this.calcKey(route)];
-    }
-
-    retrieve(route: ActivatedRouteSnapshot): DetachedRouteHandle {
+    /** Retrieves the previously stored route */
+    public retrieve(route: ActivatedRouteSnapshot): DetachedRouteHandle {
         if (!route.routeConfig) {
-          return null;
+            return null;
         }
-        return this.handlers[this.calcKey(route)];
+
+        return this.handlers[route.routeConfig.path];
     }
 
-    shouldReuseRoute(future: ActivatedRouteSnapshot, curr: ActivatedRouteSnapshot): boolean {
-        return this.calcKey(curr) === this.calcKey(future);
+    /** Determines if a route should be reused */
+    public shouldReuseRoute(future: ActivatedRouteSnapshot, curr: ActivatedRouteSnapshot): boolean {
+        return future.routeConfig === curr.routeConfig;
     }
-
 }
