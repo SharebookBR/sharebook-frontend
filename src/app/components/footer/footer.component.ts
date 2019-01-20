fb6bbec7ec01b3c0cf63ef283c57ebd262d944f5
gilvan <g.r.almeida@live.com>
2019-01-20 15:32:32 -0200

    Alterando os links dos repositórios no enum e utilizando eles no component footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,11 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Link } from '../../core/models/link';
+import { RepositoriesUrls } from '../../core/models/RepositoriesUrls';
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+
+  repositoriesLinks: Link[] = [
+    {url: RepositoriesUrls.BACKEND, content: 'Backend'},
+    {url: RepositoriesUrls.FRONTEND, content: 'Frontend'},
+    {url: RepositoriesUrls.QA, content: 'QA'},
+    {url: RepositoriesUrls.MOBILE, content: 'Mobile'}
+  ];
+
   constructor() {}
 
   ngOnInit() {}
