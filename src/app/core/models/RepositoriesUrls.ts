df0f3558f72e949431d8622d52b4d4d349009e75
gilvan <g.r.almeida@live.com>
2019-01-20 00:44:28 -0200

    Criação de serviços para renderizar os cards do componente Apoie Projeto

---
+++ src/app/core/models/RepositoriesUrls.ts
@@ -0,0 +1,6 @@
+export enum RepositoriesUrls {
+  BACKEND = 'https://github.com/SharebookBR/backend',
+  FRONTEND = 'https://github.com/SharebookBR/frontend',
+  MOBILE = 'https://github.com/SharebookBR/mobile',
+  QA = 'https://github.com/SharebookBR/qa'
+}
---
+++ src/app/core/services/careers/careers.service.ts
@@ -0,0 +1,60 @@
+import { CardItem } from '../../models/card';
+
+export class CareersService {
+  linkedinUrl: string = 'https://www.linkedin.com/in/raffacabofrio/';
+
+  public getCareers(): CardItem[] {
+    const careers: CardItem[] = [
+      {
+        image: 'assets/img/apoie-projeto/business_plan.jpg',
+        title: 'Business Plan',
+        text: 'Estratégia, motivação, liderança.<br/><br/><br/>',
+        links: [
+          {url: this.linkedinUrl, content: 'Linkedin'}
+        ]
+      },
+      {
+        image: 'assets/img/apoie-projeto/marketing.jpg',
+        title: 'Marketing',
+        text: 'Divulgação, patrocínio, parcerias, fidelização.<br/><br/>',
+        links: [
+          {url: this.linkedinUrl, content: 'Linkedin'}
+        ]
+      },
+      {
+        image: 'assets/img/apoie-projeto/vendas.jpg',
+        title: 'Vendas',
+        text: 'Tudo é venda, mesmo que não seja comercial. Ideias, engajamento.',
+        links: [
+          {url: this.linkedinUrl, content: 'Linkedin'}
+        ]
+      },
+      {
+        image: 'assets/img/apoie-projeto/rh.jpg',
+        title: 'RH',
+        text: 'Engajamento do time. Motivação.<br/><br/><br/>',
+        links: [
+          {url: this.linkedinUrl, content: 'Linkedin'}
+        ]
+      },
+      {
+        image: 'assets/img/apoie-projeto/juridico.jpg',
+        title: 'Jurídico',
+        text: 'Que cuidados devemos tomar?',
+        links: [
+          {url: this.linkedinUrl, content: 'Linkedin'}
+        ]
+      },
+      {
+        image: 'assets/img/apoie-projeto/cientista_dados.jpg',
+        title: 'Data Science',
+        text: 'Explorar. Apresentar. Prever.',
+        links: [
+          {url: this.linkedinUrl, content: 'Linkedin'}
+        ]
+      }
+    ];
+
+    return careers;
+  }
+}
