4432930c01eff566d4677918439f6ab79f99db7d
Thiago Cunha <grupocodenome@gmail.com>
2019-02-12 23:27:49 -0200

    Base para Search Results

---
+++ src/app/components/book/search-results/search-results.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SearchResultsComponent } from './search-results.component';
+import { Routes, RouterModule } from '@angular/router';
+
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [SearchResultsComponent]
+})
+export class SearchResultsModule { }
