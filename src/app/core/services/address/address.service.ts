d19e9f7926cb7836ede2f310e2f39a26ee34df8a
gilcierweb <gilcier06@yahoo.com.br>
2019-03-31 22:20:22 -0300

    migrate angular 5 to 7 and improve scroll page

--- src/app/core/services/address/address.service.ts
+++ src/app/core/services/address/address.service.ts
@@ -1,30 +1,34 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import 'rxjs/add/operator/map';
-import { Address } from '../../models/address';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+import {map} from 'rxjs/operators';
+
+import {Address} from '../../models/address';
 import * as AppConst from '../../../core/utils/app.const';
 
 @Injectable()
 export class AddressService {
 
   resultado: Address;
-  constructor(private http: HttpClient) { }
+
+  constructor(private http: HttpClient) {
+  }
 
   getAddressByPostalCode(cep: string) {
     return this.http
-      .get(AppConst.postalCodeWebService + `${cep}/json/`)
-      .map(data => this.resultado = this.convertResponseToAddress(data));
+      .get(AppConst.postalCodeWebService + `${cep}/json/`).pipe(
+        map(data => this.resultado = this.convertResponseToAddress(data)));
   }
 
   private convertResponseToAddress(addressResponse): Address {
     const address = new Address();
 
-    address.postalCode   = addressResponse.cep;
-    address.street       = addressResponse.logradouro;
-    address.complement   = addressResponse.complemento;
+    address.postalCode = addressResponse.cep;
+    address.street = addressResponse.logradouro;
+    address.complement = addressResponse.complemento;
     address.neighborhood = addressResponse.bairro;
-    address.city         = addressResponse.localidade;
-    address.state        = addressResponse.uf;
+    address.city = addressResponse.localidade;
+    address.state = addressResponse.uf;
     return address;
   }
 }
