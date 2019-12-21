7eebdff20888a167ae0b7788d7adaad60a9764d5
ErisvaldoCorreia <correiaerisvaldo@gmail.com>
2019-12-21 16:59:54 -0300

    Ajuste de erro e redirecionamento no campo de pesquisa do header

--- src/app/components/input-search/input-search.component.ts
+++ src/app/components/input-search/input-search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
@@ -9,6 +9,7 @@ import { Router } from '@angular/router';
 })
 export class InputSearchComponent implements OnInit {
   public searchForm: FormGroup;
+  public searchAlert = false;
 
   constructor(private fb: FormBuilder, private _router: Router) {}
 
@@ -19,6 +20,22 @@ export class InputSearchComponent implements OnInit {
   }
 
   public search(): void {
-    this._router.navigate(['/book/search', this.searchForm.value.paramSearch]);
+    // verifica se há alguma coisa na busca - senão houver exibe um alerta e não direciona pesquisa
+    if (this.searchForm.value.paramSearch){
+      this._router.navigate(['/book/search', this.searchForm.value.paramSearch]);
+      this.searchAlert = false;
+    } else {
+      this.searchAlert = true;
+    }
+    
+  }
+
+  @ViewChild("alert") alert: ElementRef;
+
+  // remove o alerta, e joga para a home do site ajustando o menu novamente sem o alerta
+  closeAlert() {
+    this.alert.nativeElement.classList.remove("show");
+    this._router.navigate(['/']);
+    this.searchAlert = false;
   }
 }
