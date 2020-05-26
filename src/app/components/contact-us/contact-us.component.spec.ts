401599045e5f6d07af20627a2b8b39370848e4de
William Pavei Antero <williamantero@gmail.com>
2020-05-26 20:47:52 -0300

    fix code smells

--- src/app/components/contact-us/contact-us.component.spec.ts
+++ src/app/components/contact-us/contact-us.component.spec.ts
@@ -2,18 +2,13 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgxMaskModule } from 'ngx-mask';
 import { RecaptchaFormsModule } from 'ng-recaptcha/forms';
-import {
-  RecaptchaModule,
-  RECAPTCHA_SETTINGS,
-  RecaptchaSettings,
-} from 'ng-recaptcha';
+import { RecaptchaModule } from 'ng-recaptcha';
 import { HttpClient, HttpHandler } from '@angular/common/http';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { ContactUsComponent } from './contact-us.component';
 
 import { AppConfigModule } from '../../app-config.module';
-import { ToastrService } from 'ngx-toastr';
 
 describe('ContactUsComponent', () => {
   let component: ContactUsComponent;
