22698f63e6c5f99e44d8fe1efea1d15c83b0eb99
Luciano Souza <lucianorodrigues@outlook.com>
2024-12-25 16:56:11 -0300

    Atualização do es-lint

--- src/app/core/app-routing.module.ts
+++ src/app/core/app-routing.module.ts
@@ -145,9 +145,8 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-      scrollPositionRestoration: 'enabled',
-      relativeLinkResolution: 'legacy',
-    }),
+    scrollPositionRestoration: 'enabled'
+}),
   ],
   exports: [RouterModule],
 })
